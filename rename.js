806ebbc20ccea91587b605bd7017fbfc06657dfc
fenyiwudian <laiq@cform.io>
2019-07-04 15:16:30 +0800

    rename: dash replace lowdash

--- rename.js
+++ rename.js
@@ -5,15 +5,15 @@ const fs = require('fs');
 
 const rename = (files = []) => {
     files.forEach(file => {
-        const newName = file.replace(/\/\d+_/g, '/');
+        const newName = file.replace(/\/\d+_/g, '/').replace(/_/g, '-');
         if (file !== newName) {
             fs.renameSync(file, newName);
-            const status = fs.statSync(newName);
-            if (status.isDirectory()) {
-                const subNames = fs.readdirSync(newName);
-                const subFiles = subNames.map(name => newName + '/' + name);
-                rename(subFiles);
-            }
+        }
+        const status = fs.statSync(newName);
+        if (status.isDirectory()) {
+            const subNames = fs.readdirSync(newName);
+            const subFiles = subNames.map(name => newName + '/' + name);
+            rename(subFiles);
         }
     })
 }
