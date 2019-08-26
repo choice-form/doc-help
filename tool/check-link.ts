2df786326125420504de416e66b2a762da079c37
fenyiwudian <laiq@cform.io>
2019-08-26 16:25:24 +0800

    check image dead link

--- tool/check-link.ts
+++ tool/check-link.ts
@@ -4,6 +4,9 @@
 import fs = require('fs');
 import path = require('path');
 
+/**
+ * 获取要检查的文件夹，默认是doc文件夹
+ */
 const getDirs = () => {
   let dirsArgv = process.argv.slice(2).filter(t => t.startsWith('dirs='))[0];
   if (dirsArgv) {
@@ -17,27 +20,47 @@ const getDirs = () => {
   return allDirs;
 }
 
-
+// 结果列表
 const result: string[] = [];
 
-const scanMd = (p: string) => {
+
+const checkDeadLink = (realLink: string, p: string, type: string) => {
+
+  if (realLink.startsWith('http')) {
+    return;
+  }
+  if (realLink.includes('#')) {
+    realLink = realLink.substr(0, realLink.indexOf('#'));
+  }
+  const currentP = path.dirname(p);
+  const realPath = path.resolve(currentP, realLink);
+  if (!fs.existsSync(realPath)) {
+    result.push(`文件 ./${p} 中存在${type}死链 ${realLink} `)
+  }
+}
+
+/**
+ * 扫描markdown文件检查链接
+ * @param p
+ */
+const scanMarkdown = (p: string) => {
   const text = fs.readFileSync(p).toString();
-  const reg = /\[[^\]]+\]\([^\)]+\)/g;
-  const links = text.match(reg);
-  if (links) {
-    links.forEach(link => {
+  // markdown之间的链接检查
+  const mdReg = /\[[^\]]+\]\([^\)]+\)/g;
+  const mdLinks = text.match(mdReg);
+  if (mdLinks) {
+    mdLinks.forEach(link => {
       let realLink = link.match(/\([^)]+/)[0].substr(1);
-      if (realLink.startsWith('http')) {
-        return;
-      }
-      if (realLink.includes('#')) {
-        realLink = realLink.substr(0, realLink.indexOf('#'));
-      }
-      const currentP = p.substr(0, p.lastIndexOf('/'));
-      const realPath = path.resolve(currentP, realLink);
-      if (!fs.existsSync(realPath)) {
-        result.push(`文件 ./${p} 中存在死链 ${realLink} `)
-      }
+      checkDeadLink(realLink, p, 'markdown');
+    })
+  }
+  // 和图片的检查
+  const imgReg = /src=['"].+?['"]/g;
+  const imgLinks = text.match(imgReg);
+  if (imgLinks) {
+    imgLinks.forEach(link => {
+      let realLink = link.substring(5, link.length - 1);
+      checkDeadLink(realLink, p, 'image');
     })
   }
 }
@@ -57,7 +80,7 @@ const check = (dirs = ['']) => {
       });
       check(subDirs);
     } else if (dir.endsWith('.md')) {
-      scanMd(dir);
+      scanMarkdown(dir);
     }
   })
 }
